98542e31fb8c46c6389da9225ea803d05fdc479c
Daniel Eagy <daniel.eagy@gmail.com>
2016-09-27 13:01:02 -0700

    init

---
+++ htmlConvert/htmlConvert/htmlConvert.cpp
@@ -0,0 +1,20 @@
+// htmlConvert.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+	ifstream ins;
+	ofstream outs;
+
+
+
+	ins.close();
+	outs.close();
+    return 0;
+}
+
